cc371738d6e396caabe61d3fa8e9dc0f2e95fe7a
MahBoiDeveloper <mah.boi.developer@gmail.com>
2024-03-26 22:03:15 +0300

    Добавление автоматической сборки

---
+++ zBuild.bat
@@ -0,0 +1,18 @@
+@echo off
+chcp 65001 > nul
+set mixfolder=Build\Resources\Translations\ru\MIX
+
+echo Delete build cache...
+rmdir /q /s Build > nul 2> nul
+mkdir Build
+
+echo Copy files...
+xcopy Source\* Build\* /e /y
+
+for /f "tokens=*" %%f in ('dir "%mixfolder%\" /a:d /b') do (
+	echo Compiling %%f.mix...
+	Tools\ccmix.exe --create --lmd --game=ra2 --dir "%mixfolder%\%%f" --mix "%mixfolder%\%%f.mix"
+	rmdir /q /s "%mixfolder%\%%f"
+	)
+
+echo Done. Please, check Build folder.
