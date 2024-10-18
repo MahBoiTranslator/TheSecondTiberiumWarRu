497b12dfad4ff56017c4ae5bec556f73142766a1
MahBoiDeveloper <mah.boi.developer@gmail.com>
2024-10-19 01:33:45 +0300

    Правки скрипта сборки

--- runTest.bat
+++ runTest.bat
@@ -1,5 +1,7 @@
 @echo off
 
+call zBuild.bat
+
 : Копирование файлов в папку _Test
 mkdir _Test
 xcopy /h /y /c /r /s Source\ _Test\
--- zBuild.bat
+++ zBuild.bat
@@ -1,6 +1,7 @@
 @echo off
 chcp 65001 > nul
 set mixfolder=Build\Resources\Translations\ru\MIX
+set defaultfolder=Build\Resources\Translations\en\MIX
 
 echo Delete build cache...
 rmdir /q /s Build > nul 2> nul
@@ -9,9 +10,12 @@ mkdir Build
 echo Copy files...
 xcopy Source\* Build\* /e /y
 
+mkdir %defaultfolder%
+
 for /f "tokens=*" %%f in ('dir "%mixfolder%\" /a:d /b') do (
 	echo Compiling %%f.mix...
 	Tools\ccmix.exe --create --lmd --game=ts --dir "%mixfolder%\%%f" --mix "%mixfolder%\%%f.mix"
+	copy "%mixfolder%\%%f.mix" "%defaultfolder%\%%f.mix" 
 	rmdir /q /s "%mixfolder%\%%f"
 	)
 
